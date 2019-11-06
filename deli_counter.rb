b6dc3acbc99fa31362c8354b2b524499ba52e7e1
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-06 18:24:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line(people)
+  if people.size == 0
+    puts "The line is currently empty."
+    return
+  end
+
+  queue = []
+  counter = 0
+  people.each { |names|
+    queue << "#{counter + 1}. #{names}"
+  #  puts "The line is currently: #{counter}: #{names} "
+    counter += 1
+  }
+  puts "The line is currently: " + queue.join(" ")
+end
