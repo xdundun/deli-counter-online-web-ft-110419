4645d973ca23680954998d4f70e76a0331be6db7
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-06 19:17:19 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,10 +7,20 @@ def line(people)
 
   queue = []
   counter = 0
+
   people.each { |names|
     queue << "#{counter + 1}. #{names}"
-  #  puts "The line is currently: #{counter}: #{names} "
     counter += 1
   }
   puts "The line is currently: " + queue.join(" ")
+end
+
+def take_a_number(people, name)
+  if people.size == 0
+    people.push(name)
+    puts "Welcome, #{name}. You are number 1 in line."
+  elsif people.size > 0
+    people.push(name)
+    puts "Welcome, #{name}. You are number #{people.size} in line."
+end
 end
